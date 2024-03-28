a8811361e2a003532218b918823c1d13c416fc86
김대영 <wndnjs714@gmail.com>
2024-03-28 17:00:05 +0900

    baseurl

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,5 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-	baseURL: process.env.NODE_ENV === 'production' ? process.env['HOST'] : undefined
+	baseURL: process.env['HOST']
 })
