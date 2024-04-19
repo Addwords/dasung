e789a05f2985735288083aa1727b663b0870c00f
Addwords <wndnjs714@naver.com>
2024-04-19 21:25:27 +0900

    제발...

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@
  */
 const nextConfig = {
   output: 'standalone',
-
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
 
